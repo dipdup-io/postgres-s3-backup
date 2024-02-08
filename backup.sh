86d1a9b827f3c8b27e31ea114448bcf246e17ee8
dmir <demonmir@inbox.ru>
2024-02-08 17:39:43 +0300

    Another fix

--- backup.sh
+++ backup.sh
@@ -100,8 +100,8 @@ case "${PG_BACKUP_ACTION:-dump}" in
     fi
 
     echo "Turning off autovacuum"
-    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -c "ALTER SYSTEM SET autovacuum = off;"
-    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -c "SELECT pg_reload_conf;"
+    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d postgres -U $POSTGRES_USER -c "ALTER SYSTEM SET autovacuum = off;"
+    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d postgres -U $POSTGRES_USER -c "SELECT pg_reload_conf();"
 
     echo "Downloading latest snapshot from $PG_BACKUP_FILE"
     curl -o dump.backup $PG_BACKUP_FILE
@@ -110,7 +110,7 @@ case "${PG_BACKUP_ACTION:-dump}" in
     pg_restore -v -d $POSTGRES_DB $POSTGRES_HOST_OPTS dump.backup
 
     echo "Re-enable autovacuum"
-    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -c "ALTER SYSTEM RESET autovacuum;"
-    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d $POSTGRES_DB -U $POSTGRES_USER -c "SELECT pg_reload_conf;"
+    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d postgres -U $POSTGRES_USER -c "ALTER SYSTEM RESET autovacuum;"
+    psql -h $POSTGRES_HOST -p $POSTGRES_PORT -d postgres -U $POSTGRES_USER -c "SELECT pg_reload_conf();"
     ;;
 esac
