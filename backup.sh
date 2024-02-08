a7a14a162806ecfe0f85f1a9f2449d10894bc6b1
dmir <demonmir@inbox.ru>
2024-02-08 17:20:24 +0300

    Try autovacuum

--- backup.sh
+++ backup.sh
@@ -99,10 +99,18 @@ case "${PG_BACKUP_ACTION:-dump}" in
       exit 1
     fi
 
+    echo "Turning off autovacuum"
+    psql $POSTGRES_HOST_OPTS -c "ALTER SYSTEM SET autovacuum = off;"
+    psql $POSTGRES_HOST_OPTS -c "SELECT pg_reload_conf;"
+
     echo "Downloading latest snapshot from $PG_BACKUP_FILE"
     curl -o dump.backup $PG_BACKUP_FILE
 
     echo "Restoring $POSTGRES_DB database"
     pg_restore -v -d $POSTGRES_DB $POSTGRES_HOST_OPTS dump.backup
+
+    echo "Re-enable autovacuum"
+    psql $POSTGRES_HOST_OPTS -c "ALTER SYSTEM RESET autovacuum;"
+    psql $POSTGRES_HOST_OPTS -c "SELECT pg_reload_conf;"
     ;;
 esac
